cb3120138e46d2e2b3be1e13ff612130ff711838
krajeshkumar11 <k.rajeshkumar@msitprogram.net>
2018-10-03 10:21:14 +0530

    "check_style_score -> 4 of 6 passed. style score: 1/1 "

--- ADS-1-assignment/m8/8.2 Team Ranking With Selection/8.2 Team Ranking With Selection/Solution.java
+++ ADS-1-assignment/m8/8.2 Team Ranking With Selection/8.2 Team Ranking With Selection/Solution.java
@@ -1,32 +1,85 @@
 import java.util.*;
+
+class Team {
+    String name;
+    int wins;
+    int lose;
+    int draw;
+    Team(String name, int wins, int lose, int draw) {
+        this.name = name;
+        this.wins = wins;
+        this.lose = lose;
+        this.draw = draw;
+    }
+
+    public int compareTo(Team other) {
+        if(this.wins < other.wins) {
+            return -1;
+        } else if (this.wins > other.wins) {
+            return 1;
+        } else if (this.wins == other.wins) {
+            if(this.lose < other.lose) {
+                return 1;
+            } else if (this.lose > other.lose) {
+                return -1;
+            } else if (this.lose == other.lose) {
+                if(this.draw < other.draw) {
+                    return -1;
+                } else if (this.draw > other.draw) {
+                    return 1;
+                }
+                return 0;
+            }
+        }
+        return 0;
+    }
+}
+
 public class Solution{
-    public static void sort(Comparable[] a) {
+    public static void sort(Team[] a) {
         for (int i = 0; i < a.length; i++) {
             int min = i;
             for (int j = i + 1; j < a.length; j++) {
-                if(a[j].compareTo(a[min]) < 0) {
+                if(a[j].compareTo(a[min]) >= 0) {
                     min = j;
                 }
             }
             exchange(a, i, min);
         }
-        System.out.println(Arrays.toString(a));
     }
 
-    public static void exchange(Comparable[] a, int index1, int index2) {
-        Comparable temp = a[index1];
+    public static void exchange(Team[] a, int index1, int index2) {
+        Team temp = a[index1];
         a[index1] = a[index2];
         a[index2] = temp;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        sc.nextLine();
+        Team[] teams = new Team[10];
+        int index = 0;
         while(sc.hasNext()) {
             String st = sc.nextLine();
             // {5, 7, -1, -3, 0, 2, 1}
-            Comparable[] a = st.split(" ");
-            sort(a);
+            String[] starr = st.split(",");
+            teams[index++] = new Team(starr[0], Integer.parseInt(starr[1]), Integer.parseInt(starr[2]), Integer.parseInt(starr[2]));
+        }
+        // for (int i = 0; i < index; i++) {
+        //     System.out.print(teams[i].name+",");
+        //     System.out.print(teams[i].wins+",");
+        //     System.out.print(teams[i].lose+",");
+        //     System.out.print(teams[i].draw+",");
+        //     System.out.println();
+        // }
+        Team[] copy = new Team[index];
+        System.arraycopy(teams, 0, copy, 0, index);
+        teams = copy;
+        sort(teams);
+        for (int i = 0; i < teams.length; i++) {
+            System.out.print(teams[i].name);
+            if(i < teams.length - 1) {
+                System.out.print(",");
+            }
         }
     }
 }
